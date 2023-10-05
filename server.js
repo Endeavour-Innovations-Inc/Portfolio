ebdd55850acf6797fbb2f9c978b20fd0257413a0
Endeavour-Innovations-Inc <albert.lipaev@wsu.edu>
2023-10-05 14:02:58 -0700

    Another fix hopefully it works now

--- server.js
+++ server.js
@@ -3,8 +3,20 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Serve static files from the "public/views" directory
-app.use(express.static(path.join(__dirname, 'public', 'views')));
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Explicit route for root "/"
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'views', 'index.html'));
+});
+
+// Explicit route for /software-engineering
+app.get('/software-engineering', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'views', 'software-engineering.html'));
+});
+
+// If you have other pages, you can define routes for them similarly.
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
