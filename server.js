dcf31b7611e85a72f677f70337b9641b4cc17778
Endeavour-Innovations-Inc <albert.lipaev@wsu.edu>
2023-10-05 13:19:21 -0700

    Potential frontnend fix

--- server.js
+++ server.js
@@ -3,20 +3,8 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Serve static files from the root directory
-app.use(express.static(__dirname));
-
-// Explicit route for root "/"
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'views', 'index.html'));
-});
-
-// Explicit route for /software-engineering
-app.get('/software-engineering', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'views', 'software-engineering.html'));
-});
-
-// If you have other pages, you can define routes for them similarly.
+// Serve static files from the "public/views" directory
+app.use(express.static(path.join(__dirname, 'public', 'views')));
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
