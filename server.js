2c942cccc79aa160c36d592c0d5160f3f3be3c45
Endeavour-Innovations-Inc <albert.lipaev@wsu.edu>
2023-10-05 11:27:45 -0700

    Should work on netlify now including animations

--- server.js
+++ server.js
@@ -4,24 +4,19 @@ const app = express();
 const PORT = 3000;
 
 // Serve static files from the "public" directory
-app.use(express.static('views'));
 app.use(express.static('public'));
+
+// Explicit route for root "/"
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'views', 'index.html'));
+});
 
 // Explicit route for /software-engineering
 app.get('/software-engineering', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'software-engineering.html'));
+    res.sendFile(path.join(__dirname, 'public', 'views', 'software-engineering.html'));
 });
 
-// If you have other pages like /computer-engineering and /electrical-engineering, 
-// you can similarly define explicit routes for them:
-
-// app.get('/computer-engineering', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'views', 'computer-engineering.html'));
-// });
-
-// app.get('/electrical-engineering', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'views', 'electrical-engineering.html'));
-// });
+// If you have other pages, you can define routes for them similarly.
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
